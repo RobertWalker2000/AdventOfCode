cc0ad8668d1fca1e62f5e1f14fbd5810080ebb37
Robert Walker <robertwalker2000@hotmail.com>
2022-09-21 15:37:10 +0100

    Started 2015
    Started working on the 2015 problems.
    
    Also restructured the files to differentiate between years.

---
+++ AdventOfCode/2015/Solutions/Day 1 Task 1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	//Create an input stream and open the file
+	const int day = 1;
+	const int task = 1;
+
+	std::string fileName;
+	fileName.append("2015/Inputs/D");
+	fileName.append(std::to_string(day));
+	fileName.append("T");
+	fileName.append(std::to_string(task));
+	fileName.append(".txt");
+	std::ifstream input;
+	input.open(fileName);
+
+	//Check the input stream is valid
+	if (!input)
+	{
+		std::cout << "Unable to open file" << std::endl;
+		return 1;
+	}
+
+	std::vector<char> instructions;
+	int floor = 0;
+	while (!input.eof())
+	{
+		char currentChar = input.get();
+
+		if (currentChar == '(')
+			floor++;
+		else if (currentChar == ')')
+			floor--;
+		else
+		{
+			std::cout << "ERROR:Unexpected character in input stream" << std::endl;
+			continue;
+		}
+
+		instructions.push_back(currentChar);
+	}
+
+
+	//We are done reading from the file, so close it
+	input.close();
+
+	//Output results, with a verification value to ensure every value was checked
+	std::cout << "Final floor: " << floor << std::endl;
+
+	return 0;
+}

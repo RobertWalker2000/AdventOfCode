ee1389b84ca38a611c614903c15aaf39d837c6cb
Robert Walker <robertwalker2000@hotmail.com>
2023-02-08 17:36:25 +0000

    2015 D5T1
    Completed day 5 task 1 using regex to check the strings.

---
+++ AoC_CS/Solutions/2015/D5T1.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC_CS
+{
+    class Y2015_D5T1
+    {
+        public void Solve()
+        {
+            int day = 5;
+            string filePath = @"Inputs\2015\Day " + day.ToString() + ".txt";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                System.Console.WriteLine("ERROR: File does not exist");
+                return;
+            }
+            
+            string[] input = System.IO.File.ReadAllLines(filePath);
+
+            //Regex expression to match any string which contains any of the pairs ab/cd/pq/xy
+            System.Text.RegularExpressions.Regex bannedWords = new System.Text.RegularExpressions.Regex(@"ab|cd|pq|xy");
+
+            //Regex expression to match any string containing at least 3 vowels, and also has any consecutively repeated letter
+            System.Text.RegularExpressions.Regex neededElements = new System.Text.RegularExpressions.Regex(@"(?=.*[aeiou][^aeiou]*[aeiou][^aeiou]*[aeiou])(?=.*(.)\1)");
+
+            int matches = 0;
+            foreach (string line in input)
+            {
+                if (!bannedWords.IsMatch(line))
+                    if (neededElements.IsMatch(line))
+                        matches++;
+            }
+            
+            System.Console.WriteLine("Nice words: " + matches.ToString());
+        }
+    }
+}
--- AoC_CS/Solutions/2015/Main.cs
+++ AoC_CS/Solutions/2015/Main.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var solution = new Y2015_D4T2();
+            var solution = new Y2015_D5T1();
             solution.Solve();
         }
     }

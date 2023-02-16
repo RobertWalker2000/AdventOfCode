d2e22c464aeda636398579037a279fa52e071b0c
Robert Walker <robertwalker2000@hotmail.com>
2023-02-16 21:53:47 +0000

    Trimmed 2015D11T2
    Trimmed some unnecessary code from the solution - Regex for a single set of double characters, and the return value of NextSequence being unecessary.

--- AoC_CS/Solutions/2015/D11T1.cs
+++ AoC_CS/Solutions/2015/D11T1.cs
@@ -7,14 +7,12 @@
             char[] password = "hxbxwxba".ToCharArray();
 
             System.Text.RegularExpressions.Regex doubleDoubles = new System.Text.RegularExpressions.Regex(@"(?=.*(.)\1.*(.)\2)");   //Regex to check for 2 sets of double letters
-            System.Text.RegularExpressions.Regex singleDouble = new System.Text.RegularExpressions.Regex(@"(.)\1");   //Regex to check for at least 1 set of double letters
 
             //Start by incrementing the password to its first iteration with a sequence
             int seqStart = password.Length - 3;
-            char seqEnd = NextSequence(password, ref seqStart);
 
             int passwordIterations = 0;
-            const int neededIterations = 2;
+            const int neededIterations = 1;
             while(passwordIterations < neededIterations)
             {
                 //Increment the password
@@ -29,7 +27,7 @@
                     seqStart = password.Length - 3;
 
                     //Find the next password with a sequence
-                    seqEnd = NextSequence(password, ref seqStart);
+                    NextSequence(password, ref seqStart);
                 }
 
                 //Check if the current password contains 2 sets of double letters
@@ -40,7 +38,7 @@
             System.Console.WriteLine("Password: " + new string(password));
         }
 
-        char NextSequence(char[] input, ref int seqIndex)
+        void NextSequence(char[] input, ref int seqIndex)
         {
             //Check that we can make a sequence starting at the current position
             //Sequence is impossible if starting char is y or z, as this would cause the final char to be out of bounds
@@ -48,7 +46,8 @@
             {
                 //Move back a character and try again
                 seqIndex--;
-                return NextSequence(input, ref seqIndex);
+                NextSequence(input, ref seqIndex);
+                return;
             }
 
             //If the 2nd char is more than 1 greater than the 1st, can't make a sequence without incrementing the first character
@@ -57,10 +56,11 @@
                 IncrementChar(input, seqIndex);
 
                 if (ContainsSequence(input))
-                    return input[seqIndex];
+                    return;
 
                 seqIndex = input.Length - 3;    //Reset the sequence index to check for sequences in front of the char we just incremented, since we have a fresh set to work with
-                return NextSequence(input, ref seqIndex);
+                NextSequence(input, ref seqIndex);
+                return;
             }
 
             //If the 2nd char is already in sequence, the 3rd char must be no greater than the 2nd to allow a sequence
@@ -70,10 +70,11 @@
                 IncrementChar(input, seqIndex);
 
                 if (ContainsSequence(input))
-                    return input[seqIndex];
+                    return;
 
                 seqIndex = input.Length - 3;    //Reset the seqIndex to check for sequences in front of this, as we have a new set to work with
-                return NextSequence(input, ref seqIndex);
+                NextSequence(input, ref seqIndex);
+                return;
             }
 
             //If we reached here, all characters are appropriate to generate a sequence
@@ -90,9 +91,10 @@
             for (int i = seqIndex + 3; i < input.Length; i++)
                 input[i] = 'a';
 
-            return input[seqIndex + 2];
+            return;
         }
 
+        //Checks if the string contains 3 sequential characters anywehere within it
         bool ContainsSequence(char[] password)
         {
             int seqLength = 0;
@@ -112,6 +114,7 @@
             return false;
         }
 
+        //Increments the char at a given index and all of its rollovers
         void IncrementChar(char[] input, int index)
         {
             //Start at the character to be incremented, working backwards whenever a character wraps around

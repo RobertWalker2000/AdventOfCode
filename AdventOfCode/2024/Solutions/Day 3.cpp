20f3c6a8a8138077fb219b5ca7cdd4fa4b95b460
RobertWalker2000 <55134510+RobertWalker2000@users.noreply.github.com>
2024-12-03 16:45:06 +0000

    2024 Day 3 Part 2

--- AdventOfCode/2024/Solutions/Day 3.cpp
+++ AdventOfCode/2024/Solutions/Day 3.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 int EvaluateLine(std::string line);
+int EvaluateLineConditional(std::string line, bool* condition);
 int EvaluateInstruction(std::string instruction);
 
 int main()
@@ -24,17 +25,21 @@ int main()
 		exit(1);
 	}
 
-	int result = 0;
+	bool doMul = true;
+	int raw = 0;
+	int conditionalResult = 0;
 	while (!input.eof())
 	{
 		static const int lineSize = 4096;
 		char line[lineSize];
 		input.getline(line, lineSize);
 		std::string strLine(line);
-		result += EvaluateLine(strLine);
+		raw += EvaluateLine(strLine);
+		conditionalResult += EvaluateLineConditional(strLine, &doMul);
 	}
 
-	std::cout << "Final result: " << result << std::endl;
+	std::cout << "Raw result: " << raw << std::endl;
+	std::cout << "Conditional result: " << conditionalResult << std::endl;
 	exit(0);
 }
 
@@ -44,13 +49,13 @@ int EvaluateLine(std::string line)
 	while (!line.empty())
 	{
 		//Find the first instance of an "m", marking the start of a potential instruction
-		int m = line.find('m');
+		int mul = line.find("mul(");
 
 		//If there is no M, no instructions remain
-		if (m == std::string::npos)
+		if (mul == std::string::npos)
 			return value;
 
-		line.erase(0, m);	//Erase all characters before the M
+		line.erase(0, mul);	//Erase all characters before the M
 		
 		//Find the first ')', marking the end of a potential instruction
 		int instrEnd = line.find(')');
@@ -58,6 +63,73 @@ int EvaluateLine(std::string line)
 		//If there is no ')', no instructions remain
 		if (instrEnd == std::string::npos)
 			return value;
+
+		//Evaluate the potential instruction
+		value += EvaluateInstruction(line.substr(0, instrEnd + 1));
+
+		//Remove the M, so we can start looking for the next instruction
+		line.erase(0, 1);
+	}
+
+	return value;
+}
+
+int EvaluateLineConditional(std::string line, bool* condition)
+{
+	int value = 0;
+	while (!line.empty())
+	{
+		//If we are currently set to not do instrucitons, we can ignore everything until the next "Do()"
+		if (*condition == false)
+		{
+			int nextDo = line.find("do()");
+
+			//If there is no Do() instruciton, there is nothing else to evaluate
+			if (nextDo == std::string::npos)
+				break;
+
+			//If we found a Do(), remove everything before it and set our condition to true
+			line.erase(0, nextDo);
+			*condition = true;
+		}
+
+		//Find the first instance of a "mul(", marking the start of a potential instruction
+		int mul = line.find("mul(");
+
+		int nextDont = line.find("don't()");
+
+		//If we didn't find a mul or a dont instruction, there is nothing left to evaluate
+		if (mul == std::string::npos && nextDont == std::string::npos)
+			break;
+
+		//If we only found a don't instruction, erase everyhting before it, set condition to false, and go to the next loop
+		if (mul == std::string::npos)
+		{
+			line.erase(0, nextDont);
+			*condition = false;
+			continue;
+		}
+
+		//If we found both instructions, but the Dont was first, do as above
+		if (nextDont != std::string::npos)
+		{
+			if (nextDont < mul)
+			{
+				line.erase(0, nextDont);
+				*condition = false;
+				continue;
+			}
+		}
+
+		//If we have reached here, our next instruction is a mul. Begin evaluating
+		line.erase(0, mul);	//Erase all characters before the "mul("
+
+		//Find the first ')', marking the end of a potential instruction
+		int instrEnd = line.find(')');
+
+		//If there is no ')', no instructions remain
+		if (instrEnd == std::string::npos)
+			break;
 
 		//Evaluate the potential instruction
 		value += EvaluateInstruction(line.substr(0, instrEnd + 1));

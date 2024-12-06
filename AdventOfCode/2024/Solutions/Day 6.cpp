233c533d0d09479b1aa92d50d43f3408ca8e6f6e
RobertWalker2000 <55134510+RobertWalker2000@users.noreply.github.com>
2024-12-06 17:56:27 +0000

    2024 Day 6 Task 2 (SLOW)
    Solution for part 2 is very slow, as it needs to calculate the full path for every position calculated in part 1.
    
    Each loop is still relatively fast, but the incredibly high number of loops required (>5000) leads to a long execution time.

--- AdventOfCode/2024/Solutions/Day 6.cpp
+++ AdventOfCode/2024/Solutions/Day 6.cpp
@@ -4,14 +4,19 @@
 #include <vector>
 #include <set>
 
-int CalculateUniqueSpaces(std::vector<std::string>* map, std::pair<int, int> pos, int moveX = 0, int moveY = -1);
-bool IsPosInBounds(std::pair<int, int> pos, std::vector<std::string>* map);
+using Coords = std::pair<int, int>;
+using DirCoords = std::pair < std::pair<int, int>, std::pair<int, int>>;
+
+std::set<Coords> CalculateUniqueSpaces(std::vector<std::string>* map, Coords pos);
+int CalculateLoopingObstacles(std::vector<std::string>* map, std::set<Coords> possibleObstructions, Coords startPos);
+bool DoesMapLoop(std::vector<std::string>* map, Coords pos);
+bool IsPosInBounds(Coords pos, std::vector<std::string>* map);
 void TurnRight(int* x, int* y);
 
 int main()
 {
 	//Create an input stream and open the file
-	const int day = 0;
+	const int day = 6;
 
 	std::string fileName;
 	fileName.append("2024/Inputs/D");
@@ -30,7 +35,7 @@ int main()
 	//Read the map into a vector, searching for the starting position as we go
 	std::vector<std::string> map;
 	int row = 0;
-	std::pair<int, int> pos;
+	Coords pos;
 	while (!input.eof())
 	{
 		char temp[256];
@@ -43,22 +48,27 @@ int main()
 		map.push_back(line);	//Add the line to the map
 		int col = line.find('^');	//Look for starting pos
 		if (col != std::string::npos)
-			pos = std::pair<int, int>(col, row);	//If we find start pos, store it
+			pos = Coords(col, row);	//If we find start pos, store it
 
 		row++;
 	}
 
-	int uniquePos = CalculateUniqueSpaces(&map, pos);
+	std::set<Coords> uniquePositions = CalculateUniqueSpaces(&map, pos);
+	int possibleLoops = CalculateLoopingObstacles(&map, uniquePositions, pos);
 
-	std::cout << "Unique positions visited: " << uniquePos << std::endl;
+	std::cout << "Unique positions visited: " << uniquePositions.size() << std::endl;
+	std::cout << "Potential ways to create aloop: " << possibleLoops << std::endl;
 	exit(0);
 }
 
-int CalculateUniqueSpaces(std::vector<std::string>* map, std::pair<int, int> pos, int moveX, int moveY)
+std::set<Coords> CalculateUniqueSpaces(std::vector<std::string>* map, Coords pos)
 {
 	//Set allows us to easily track visited positions and check if they already existed
-	std::set<std::pair<int, int>> uniquePositions;
+	std::set<Coords> uniquePositions;
 	static const char obstacle = '#';
+
+	int moveX = 0;
+	int moveY = -1;
 
 	//Keep looping until we leave the map
 	while (IsPosInBounds(pos, map))
@@ -66,7 +76,7 @@ int CalculateUniqueSpaces(std::vector<std::string>* map, std::pair<int, int> pos
 		uniquePositions.insert(pos);
 
 		//Calculate the position form after we take a step
-		std::pair<int, int> nextPos(pos.first + moveX, pos.second + moveY);
+		Coords nextPos(pos.first + moveX, pos.second + moveY);
 		if (!IsPosInBounds(nextPos, map))
 			break;
 
@@ -81,17 +91,17 @@ int CalculateUniqueSpaces(std::vector<std::string>* map, std::pair<int, int> pos
 		pos = nextPos;
 	}
 
-	return uniquePositions.size();
+	return uniquePositions;
 }
 
-bool IsPosInBounds(std::pair<int, int> pos, std::vector<std::string>* map)
+bool IsPosInBounds(Coords pos, std::vector<std::string>* map)
 {
 	//Check if we are on a valid row (In bounds vertically)
 	if (pos.second < 0 || pos.second >= map->size())
 		return false;
 
 	//Knowing that we are on a valid row, check if we are within the bounds of that row
-	if (pos.first < 0 || pos.first > map->at(pos.second).size())
+	if (pos.first < 0 || pos.first >= map->at(pos.second).size())
 		return false;
 
 	//Passed all bounds checks, position is valid
@@ -127,4 +137,74 @@ void TurnRight(int* x, int* y)
 	*x = 0;
 	*y = -1;
 	return;
+}
+
+int CalculateLoopingObstacles(std::vector<std::string>* map, std::set<Coords> possibleObstructions, Coords startPos)
+{
+	//We can't obstruct the starting position, so remove it form the set
+	possibleObstructions.erase(startPos);
+
+	//Obstruction would only make a difference if it is on the existing path
+	//Loop through all existing positions and check if an obstruction in that position creates a loop
+	int loops = 0;
+	static const char obstruction = '#';
+	static const char clearSpace = '.';
+	for (std::set<Coords>::iterator it = possibleObstructions.begin(); it != possibleObstructions.end(); it++)
+	{
+		//Update the current posiiton to be an obstruction
+		map->at(it->second).at(it->first) = obstruction;
+
+		//Check if the new map loops
+		if (DoesMapLoop(map, startPos))
+			loops++;
+
+		//Remove the new obstruction to restore the original map
+		map->at(it->second).at(it->first) = clearSpace;
+	}
+
+	return loops;
+}
+
+bool DoesMapLoop(std::vector<std::string>* map, Coords pos)
+{
+	//A set that tracks the positions we have visited, and the direction we were facing
+	//If we ever end up in the same position *and* direction, we are in a loop
+	std::set<DirCoords> stepsTaken;
+
+	int moveX = 0;
+	int moveY = -1;
+
+	static const char obstacle = '#';
+
+	//Keep looping until we leave the map
+	while (IsPosInBounds(pos, map))
+	{
+		//Create a DirCoords that represents the current position and direction
+		Coords dir(moveX, moveY);
+		DirCoords dc;
+		dc.first = pos;
+		dc.second = dir;
+
+		//Try inserting this DirCoords into the set. If it already exists, we are in a loop
+		if (!stepsTaken.insert(dc).second)
+			return true;
+
+		//Calculate the position form after we take a step
+		Coords nextPos(pos.first + moveX, pos.second + moveY);
+		if (!IsPosInBounds(nextPos, map))
+			break;
+
+		//If we are about to hit an abstacle, turn right
+		if (map->at(nextPos.second).at(nextPos.first) == obstacle)
+		{
+			TurnRight(&moveX, &moveY);
+			continue;	//Go back to loop again in case we need to do multiple turns, or we are about to exit the map now
+		}
+
+		//Our path is clear, take a step and record the position in the set
+		pos = nextPos;
+	}
+
+	//We exited the map without entering a loop
+	return false;
 }

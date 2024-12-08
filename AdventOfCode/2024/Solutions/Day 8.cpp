23908037c8874fb3f7c1a7341f4fd3368b3ea9f8
RobertWalker2000 <55134510+RobertWalker2000@users.noreply.github.com>
2024-12-08 20:42:01 +0000

    2024 Day 8 Task 1

--- AdventOfCode/2024/Solutions/Day 8.cpp
+++ AdventOfCode/2024/Solutions/Day 8.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,8 +8,10 @@
 using Coords = std::pair<int, int>;
 using Antenna = std::pair<char, Coords>;
 
-std::set<Coords> CalculateAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds);
+std::set<Coords> CalculateShortAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds);
+std::set<Coords> CalculateLongAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds);
 Coords CalculateDifference(Coords antA, Coords antB);
+Coords Simplify2DVector(Coords vec);
 bool IsInBounds(Coords pos, Coords bounds);
 
 int main()
@@ -59,15 +62,18 @@ int main()
 	//Create a coordinate representing the bottom-right-most valid coordinate
 	Coords bounds(lineLength - 1, y - 1);
 
-	std::set<Coords> antinodes = CalculateAntinodes(antennas, frequencies, bounds);
+	std::set<Coords> antinodes = CalculateShortAntinodes(antennas, frequencies, bounds);
+	std::set<Coords> resonateAntinodes = CalculateLongAntinodes(antennas, frequencies, bounds);
 
 	int numAntinodes = antinodes.size();
+	int numResAntinodes = resonateAntinodes.size();
 
-	std::cout << "Positions containing antinodes: : " << numAntinodes << std::endl;
+	std::cout << "Positions containing simple antinodes: : " << numAntinodes << std::endl;
+	std::cout << "Positions containing resonate antinodes: : " << numResAntinodes << std::endl;
 	exit(0);
 }
 
-std::set<Coords> CalculateAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds)
+std::set<Coords> CalculateShortAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds)
 {
 	//Create a setto store all the unique antinodes
 	std::set<Coords> antinodes;
@@ -102,6 +108,54 @@ std::set<Coords> CalculateAntinodes(std::multimap<char, Coords> antennas, std::s
 	return antinodes;
 }
 
+std::set<Coords> CalculateLongAntinodes(std::multimap<char, Coords> antennas, std::set<char> frequencies, const Coords bounds)
+{
+	//Create a setto store all the unique antinodes
+	std::set<Coords> antinodes;
+
+	//Loop through all the frequencies, checking for all antennas of that type
+	for (std::set<char>::iterator freq = frequencies.begin(); freq != frequencies.end(); freq++)
+	{
+		std::pair<std::multimap<char, Coords>::iterator, std::multimap<char, Coords>::iterator>  freqRange = antennas.equal_range(*freq);
+		//Loop through all the antennas, checking for antinodes with each subsequent antenna of the same type
+		for (std::multimap<char, Coords>::iterator antA = freqRange.first; antA != freqRange.second; antA++)
+		{
+			//Initialise antB to equal antA, and then increment it at the entry to the loop so we are starting with them offset by 1
+			std::multimap<char, Coords>::iterator antB = antA;
+			for (antB++; antB != freqRange.second; antB++)
+			{
+				//Calculate the offset between the antennas and a potential antinode
+				Coords diff = CalculateDifference(antA->second, antB->second);
+				diff = Simplify2DVector(diff);	//Simplify the vector down to its shortest form
+
+				//Start at antA, and move backwards along the vector until we go out of bounds
+				Coords node = antA->second;
+				while (IsInBounds(node, bounds))
+				{
+					node.first -= diff.first;
+					node.second -= diff.second;
+				}
+
+				//Node is now out of bounds, so move it once to come back in bounds
+				node.first += diff.first;
+				node.second += diff.second;
+
+				//Node is now on the first valid space for this pair of antennas
+				//Note its position while continuously moving along the vector until we go out of bounds on the other side
+				while (IsInBounds(node, bounds))
+				{
+					antinodes.insert(node);
+
+					node.first += diff.first;
+					node.second += diff.second;
+				}
+			}
+		}
+	}
+
+	return antinodes;
+}
+
 Coords CalculateDifference(const Coords antA, const Coords antB)
 {
 	Coords result(0, 0);
@@ -111,6 +165,18 @@ Coords CalculateDifference(const Coords antA, const Coords antB)
 	result.second = antB.second - antA.second;
 
 	return result;
+}
+
+Coords Simplify2DVector(Coords vec)
+{
+	int x = vec.first;
+	int y = vec.second;
+	int denom = std::gcd(abs(x), abs(y));
+
+	x = x / denom;
+	y = y / denom;
+
+	return Coords(x, y);
 }
 
 bool IsInBounds(const Coords pos, const Coords bounds)
